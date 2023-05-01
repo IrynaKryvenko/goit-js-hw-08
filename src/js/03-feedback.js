9b168896da5ed5e4bb887141a3790fba59a82c78
IrynaKryvenko <kryvenkoirina93@gmail.com>
2023-05-01 22:46:44 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,9 +3,9 @@ import throttle from 'lodash.throttle';
 const STORAGE_KEY = 'feedback-form-state';
 let formData = {};
 const refs = {
-    form: document.querySelector('.feedback-form'),
-    input: document.querySelector('input'),
-    textarea: document.querySelector('textarea'),
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
 };
 
 refs.form.addEventListener('submit', onFormSubmit);
@@ -14,25 +14,25 @@ refs.form.addEventListener('input', throttle(onFormInput, 500));
 updatePage();
 
 function onFormSubmit(evt) {
-    evt.preventDefault();
-    if (input.value === "" || textarea.value === "") {
-        return alert("Please fill in all the fields!");
-    }
-    evt.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY);
-};
+  evt.preventDefault();
+  if (refs.input.value === '' || refs.textarea.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
 
 function onFormInput(evt) {
-    formData[evt.target.name] = evt.target.value;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(obj));
-};
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
 
 function updatePage() {
-    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-    if (savedData) {
-        input.value = savedData.email || "";
-        formData.email = savedDta.email || "";
-        textarea.value = saveaData.message || "";
-        formData.message = savedData.message || ""; 
-    }
-};
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedData) {
+    refs.input.value = savedData.email || '';
+    formData.email = savedData.email || '';
+    refs.textarea.value = savedData.message || '';
+    formData.message = savedData.message || '';
+  }
+}
