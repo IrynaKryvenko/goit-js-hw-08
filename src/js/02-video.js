be3729dffdc0f5a1535d39134689953051eff2f0
IrynaKryvenko <kryvenkoirina93@gmail.com>
2023-05-01 22:59:09 +0300

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,37 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const onTimeupdate = function ({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+player.on('timeupdate', throttle(onTimeupdate, 1000));
+const timeStart = localStorage.getItem('videoplayer-current-time');
+
+if (timeStart) {
+  player
+    .setCurrentTime(timeStart)
+    .then(function (seconds) {
+  
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          break;
+
+        default:
+          break;
+      }
+    });
+}
