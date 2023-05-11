62dac89558548db1565a42b4d8233418c9c4c88c
IrynaKryvenko <kryvenkoirina93@gmail.com>
2023-05-11 14:49:24 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,21 +1,21 @@
-import Vimeo from '@vimeo/player';
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo(iframe);
+const player = new Player(iframe);
 
-const onPlay = function () {
+player.on('play', function () {
   console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const onTimeupdate = function ({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
 };
 
+player.on('timeupdate', throttle(onTimeupdate, 1000));
 
-const onTimeupdate = throttle(function ({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds);
-}, 1000);
-
-
-player.on('play', onPlay);
-player.on('timeupdate', onTimeupdate);
-
-const currentTime = JSON.parse(localStorage.getItem('videoplayer-current-time')) || 0;
-player.setCurrentTime(currentTime);
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
