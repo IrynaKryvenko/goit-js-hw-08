b86f96c6316f32bbaa69cf0c1d8f0861611c9bb3
IrynaKryvenko <kryvenkoirina93@gmail.com>
2023-05-11 16:34:33 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,44 +1,55 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-const formData = {};
 
-
-const savedFormData = JSON.parse(localStorage.getItem('feedback-form-state'));
-if (savedFormData) {
-  form.email.value = savedFormData.email || '';
-  form.message.value = savedFormData.message || '';
-}
-
-const onInputFill = throttle(() => {
-  formData.email = form.email.value;
-  formData.message = form.message.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-}, 500);
-
-form.addEventListener('input', onInputFill);
-
-
-form.addEventListener('submit', (event) => {
+function onFormSubmit(event) {
   event.preventDefault();
 
-  
-  const email = form.email.value.trim();
-  const message = form.message.value.trim();
-  if (!email || !message) {
-    console.log('Please fill out all required fields.');
-    return;
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const message = formElements.message.value;
+
+  function sendForm() {
+    const formData = {
+      email,
+      message,
+    };
+    console.log(formData);
+    event.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
   }
 
- 
-  form.reset();
-  localStorage.removeItem('feedback-form-state');
+  sendForm();
+};
 
+function onInputFill(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
 
-  const submittedFormData = {
-    email,
-    message,
-  };
-  console.log(submittedFormData);
+const formData = {};
+const savedInLocal = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+if (savedInLocal) {
+  form.email.value = savedInLocal.email;
+  form.message.value = savedInLocal.message;
+};
+
+function validateForm() {
+  const email = form.email.value.trim();
+  const message = form.message.value.trim();
+
+  if (!email || !message) {
+    alert('Attention! All form fields must be filled in!');
+    return false;
+  }
+
+  return true;
+};
+
+form.addEventListener('input', throttle(onInputFill, 500));
+form.addEventListener('submit', function (event) {
+  if (validateForm()) {
+    onFormSubmit(event);
+  }
 });
-
