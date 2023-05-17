ab2c1528c0ef202325cd6e62909aa1b762be30b3
IrynaKryvenko <kryvenkoirina93@gmail.com>
2023-05-17 19:05:37 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,18 +4,25 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function () {
+const currentTimeKey = 'videoplayer-current-time';
+
+player.on('play', function() {
   console.log('played the video!');
 });
 
-player.getVideoTitle().then(function (title) {
+player.getVideoTitle().then(function(title) {
   console.log('title:', title);
 });
 
-const onTimeupdate = function ({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds);
-};
+const onTimeUpdate = throttle(function(event) {
+  localStorage.setItem(currentTimeKey, event.seconds);
+}, 1000);
 
-player.on('timeupdate', throttle(onTimeupdate, 1000));
+player.on('timeupdate', onTimeUpdate);
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+const savedTime = localStorage.getItem(currentTimeKey);
+if (savedTime) {
+  player.setCurrentTime(savedTime).catch(function(error) {
+    console.log('Failed to set current time:', error);
+  });
+}
